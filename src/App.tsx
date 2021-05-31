bb51c441f5579afffe422e3664c7b224ee9708be
Seungse Oh <ohsse12521@gmail.com>
2021-05-31 23:25:14 +0900

    #8.9 GlobalStyles on Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import { useReactiveVar } from "@apollo/client";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
-import { ourTheme } from "./styles";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
 import { darkModeVar, isLoggedInVar } from "./apollo";
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 
-const lightTheme = {
-  fontColor: "#2c2c2c",
-  bgColor: "lightgray",
-};
-
-const darkTheme = {
-  fontColor: "lightgray",
-  bgColor: "#2c2c2c",
-};
 
 /* interface IContainerProps {
   floating: boolean;
@@ -32,6 +23,7 @@ const App = () => {
   const darkMode = useReactiveVar(darkModeVar);
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
       <Router>
         <Switch>
           <Route path="/" exact>
--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,25 @@
 import { DefaultTheme } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 export const ourTheme: DefaultTheme = {
   bgColor: "black",
   fontColor: "blue",
 };
+
+export const lightTheme = {
+  fontColor: "#2c2c2c",
+  bgColor: "lightgray",
+};
+
+export const darkTheme = {
+  fontColor: "lightgray",
+  bgColor: "#2c2c2c",
+};
+
+export const GlobalStyles = createGlobalStyle`
+    ${reset}
+    body {
+        background-color: ${(props) => props.theme.bgColor};
+    }
+`;
