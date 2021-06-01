77f08b1529d1fb1355a79e06934a43ca6b311b0f
SeungseOh <ohsse12521@gmail.com>
2021-06-01 11:34:09 +0900

    #10.1 Login UI Clone

--- src/styles.ts
+++ src/styles.ts
@@ -3,18 +3,29 @@ import reset from "styled-reset";
 
 export const lightTheme: DefaultTheme = {
   fontColor: "#2c2c2c",
-  bgColor: "lightgray",
+  bgColor: "white",
 };
 
 export const darkTheme: DefaultTheme = {
-  fontColor: "lightgray",
+  fontColor: "white",
   bgColor: "#2c2c2c",
 };
 
 export const GlobalStyles = createGlobalStyle`
     ${reset}
+    input {
+      all:unset;
+    }
+    * {
+      box-sizing:border-box;
+    }
     body {
-        background-color: ${(props) => props.theme.bgColor};
+      background-color: #FAFAFA;
+        font-size:14px;
+        font-family:'Open Sans', sans-serif;
+    }
+    a {
+      text-decoration: none;
     }
 `;
 
