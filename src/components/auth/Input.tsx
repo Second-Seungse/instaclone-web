0afea2aa95f33b4d17336d6cc83d2a4abe3f3ebc
SeungseOh <ohsse12521@gmail.com>
2021-06-02 01:35:36 +0900

    #10.8 React Hook Form is Awesome

--- src/components/auth/Input.tsx
+++ src/components/auth/Input.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
-type InputProps = Omit<JSX.IntrinsicElements["input"], "ref">;
+// * 속성
+type InputProps = {
+  hasError?: Boolean;
+};
+type InputRef = InputProps & Omit<JSX.IntrinsicElements["input"], "ref">;
 
-const SInput = styled.input`
+const SInput = styled.input<InputProps>`
   width: 100%;
   border-radius: 3px;
   padding: 7px;
   background-color: #fafafa;
-  border: 0.5px solid ${(props) => props.theme.borderColor};
+  border: 0.5px solid
+    ${(props) => (props.hasError ? "tomato" : props.theme.borderColor)};
   margin-top: 5px;
   box-sizing: border-box;
   &::placeholder {
     font-size: 12px;
   }
+  &:focus {
+    border-color: rgb(38, 38, 38);
+  }
 `;
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+const Input = React.forwardRef<HTMLInputElement, InputRef>(
   ({ ...props }, forwardedRef) => {
     return <SInput ref={forwardedRef} {...props} />;
   }
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -9,6 +9,7 @@ import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
 import Button from "../components/auth/Button";
 import FormBox from "../components/auth/FormBox";
+import FormError from "../components/auth/FormError";
 import Input from "../components/auth/Input";
 import Separator from "../components/auth/Separator";
 import PageTitle from "../components/pageTitle";
@@ -28,12 +29,15 @@ type LoginForm = {
 };
 
 const Login = () => {
-  const { register, handleSubmit, getValues, setValue } = useForm<LoginForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<LoginForm>({
+    mode: "onBlur",
+  });
   const onSubmitValid: SubmitHandler<LoginForm> = (data) => {
     console.log(data);
-  };
-  const onSubmitInvalid: SubmitErrorHandler<LoginForm> = (data) => {
-    console.log(data, "invalid");
   };
   return (
     <AuthLayout>
@@ -42,20 +46,31 @@ const Login = () => {
         <div>
           <FontAwesomeIcon icon={faInstagram} size="3x" />
         </div>
-        <form onSubmit={handleSubmit(onSubmitValid, onSubmitInvalid)}>
+        <form onSubmit={handleSubmit(onSubmitValid)}>
           <Input
-            {...register("username", { minLength: 5, required: true })}
+            {...register("username", {
+              minLength: {
+                value: 5,
+                message: "Username should be longer than 5 chars.",
+              },
+              required: true,
+            })}
             name="username"
             type="text"
             placeholder="Username"
+            hasError={Boolean(errors?.username?.message)}
           />
+          <FormError message={errors?.username?.message} />
           <Input
             {...register("password", { required: true })}
             name="password"
             type="password"
             placeholder="Password"
+            
+            hasError={Boolean(errors?.password?.message)}
           />
-          <Button type="submit" value="Log in" />
+          <FormError message={errors?.password?.message} />
+          <Button type="submit" value="Log in" disabled={!isValid} />
         </form>
         <Separator />
         <FacebookLogin>
