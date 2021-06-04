87be265f654526323e83fe42cd491f85e8cd8640
SeungseOh <ohsse12521@gmail.com>
2021-06-04 13:35:54 +0900

    #11.5 Photo Component part Two

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,9 +1,14 @@
+import { type } from "os";
 import styled from "styled-components";
 
-const SAvatar = styled.div`
-  width: 25px;
-  height: 25px;
-  border-radius: 15px;
+type AvatarForm = {
+  lg: boolean
+}
+
+const SAvatar = styled.div<AvatarForm>`
+  width: ${(props) => (props.lg ? "30px" : "25px")};
+  height: ${(props) => (props.lg ? "30px" : "25px")};
+  border-radius: 50%;
   background-color: #2c2c2c;
   overflow: hidden;
 `;
@@ -12,7 +17,7 @@ const Img = styled.img`
   max-width: 100%;
 `;
 
-function Avatar({ url = "" }) {
-  return <SAvatar>{url !== "" ? <Img src={url} /> : null}</SAvatar>;
-}
+const Avatar = ({ url = "", lg = false }) => {
+  return <SAvatar lg={lg}>{url !== "" ? <Img src={url} /> : null}</SAvatar>;
+};
 export default Avatar;
