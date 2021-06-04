911f296e7f10421235e8c083abee00760bdbbb93
SeungseOh <ohsse12521@gmail.com>
2021-06-04 11:36:28 +0900

    #11.3 Avatar

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -6,6 +6,7 @@ import { me } from "../__generated__/me";
 const ME_QUERY = gql`
   query me {
     me {
+      id
       username
       avatar
     }
@@ -17,13 +18,12 @@ const useUser = () => {
   const { data } = useQuery<me>(ME_QUERY, {
     skip: !hasToken,
   });
-  console.log(data);
   // * useEffect는 hook이 마운트되면 한 번 실행되고, 데이터가 변경될 때 마다 실행된다
   useEffect(() => {
     if (data?.me === null) {
-        logUserOut();
+      logUserOut();
     }
   }, [data]);
-  return;
+  return { data };
 };
 export default useUser;
