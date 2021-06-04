df950f8c8c7e8c42a7d6c7df9b321b21100d25d0
SeungseOh <ohsse12521@gmail.com>
2021-06-04 09:55:48 +0900

    #11.1 Header part Two

---
+++ src/hooks/useUser.tsx
@@ -0,0 +1,22 @@
+import { gql, useQuery, useReactiveVar } from "@apollo/client";
+import { isLoggedInVar } from "../apollo";
+import { me } from "../__generated__/me";
+
+const ME_QUERY = gql`
+  query me {
+    me {
+      username
+      avatar
+    }
+  }
+`;
+
+function useUser() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const { data, error } = useQuery<me>(ME_QUERY, {
+    skip: !isLoggedIn,
+  });
+  console.log(data, error);
+  return;
+}
+export default useUser;
