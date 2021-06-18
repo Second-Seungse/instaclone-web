0bd2c632a9205944e02d84328c01d20e633b5822
SeungseOh <ohsse12521@gmail.com>
2021-06-18 20:44:40 +0900

    Cannot find file '../components/PageTitle' in './src/screens'

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -15,7 +15,6 @@ import FormBox from "../components/auth/FormBox";
 import FormError from "../components/auth/FormError";
 import Input from "../components/auth/Input";
 import Separator from "../components/auth/Separator";
-import PageTitle from "../components/PageTitle";
 import routes from "../routes";
 import { login, loginVariables } from "../__generated__/login";
 
@@ -101,7 +100,6 @@ const Login = () => {
 
   return (
     <AuthLayout>
-      <PageTitle title="Login" />
       <FormBox>
         <div>
           <FontAwesomeIcon icon={faInstagram} size="3x" />
